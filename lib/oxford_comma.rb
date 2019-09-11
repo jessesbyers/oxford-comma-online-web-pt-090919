5e5f1a6906bdf0ab8ad32ee2fc160c13a06875c6
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-11 18:07:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,14 @@ def oxford_comma(array)
   new_array = []
   final_array = []
   new_array = array.join(", ")
-  new_array.each do |x|
-    final_array << new_array[-1] = "and #{x}"
 end
-final_array.to_s
+
+
+
+
+#  new_array.each do |x|
+#    final_array << new_array[-1] = "and #{x}"
+#end
+#final_array.to_s
 #  binding.pry
-end
+#end
